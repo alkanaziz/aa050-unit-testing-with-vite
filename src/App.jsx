0943fab8acc7f7ae2a5baf672c2c8b6e5bbab2cd
alkanaziz <alkanaziz@gmail.com>
2024-03-27 11:46:30 +0100

    Add user list to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,35 @@
-import './App.css'
+import "./App.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          "https://jsonplaceholder.typicode.com/users"
+        );
+        const _users = await response.json();
+        setUsers(_users);
+        console.log(_users);
+      } catch (error) {
+        console.log("Error beim Fetchen der Daten: ", error);
+      }
+    };
+    fetchData();
+  }, []);
 
   return (
     <>
-      <h1>Hello</h1>
+      <h1>User List</h1>
+      <ol className="userlist">
+        {users.map((user) => {
+          return <li key={user.id}>{user.name}</li>;
+        })}
+      </ol>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
